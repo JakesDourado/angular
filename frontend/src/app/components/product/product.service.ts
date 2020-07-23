e77c07f3cf23e3133320d249e8195993df4b5322
jakes <fjakis@gmail.com>
2020-07-23 16:03:20 -0300

    exibir mensagem de erro quando não tiver comunicação com o back ou db

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,8 +1,9 @@
+import { map, catchError } from 'rxjs/operators';
 import { Product } from './product.model';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar'
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, EMPTY } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,34 +16,49 @@ export class ProductService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'x', {
       duration: 3000,
       horizontalPosition: "right",
-      verticalPosition: "top"
+      verticalPosition: "top",
+      panelClass: isError ? ['msg-error'] : ['msg-success']
     })
   }
 
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseURl, product);
+    return this.http.post<Product>(this.baseURl, product).pipe(
+      map((obj) => obj), //isso é pra mostrar a mensagem de erro
+      catchError (e => this.errorHandler(e) )
+    );
   }
+ 
 
   read(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseURl);
+    return this.http.get<Product[]>(this.baseURl).pipe(
+      map((obj) => obj), //isso é pra mostrar a mensagem de erro
+      catchError (e => this.errorHandler(e) ));
   }
 
   readById(id: number): Observable<Product> {
     const url = `${this.baseURl}/${id}`;
-    return this.http.get<Product>(url);;
+    return this.http.get<Product>(url);
   }
 
   updata(product: Product): Observable<Product> {
     const url = `${this.baseURl}/${product.id}`;
-    return this.http.put<Product>(url, product);
+    return this.http.put<Product>(url, product).pipe(
+      map((obj) => obj), //isso é pra mostrar a mensagem de erro
+      catchError (e => this.errorHandler(e) ));
   }
 
   delete(id: number): Observable<Product> {
     const url = `${this.baseURl}/${id}`;
-    return this.http.delete<Product>(url);
+    return this.http.delete<Product>(url).pipe(
+      map((obj) => obj), //isso é pra mostrar a mensagem de erro
+      catchError (e => this.errorHandler(e) ));
+  }
+  errorHandler(e: any): Observable<any>{
+    this.showMessage('Erro de comunicação, processo não enviado ', true)
+ return EMPTY
   }
 }
