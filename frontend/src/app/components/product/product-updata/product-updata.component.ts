3601ce1792449802abc3107a75a1ee516a8c9ad6
jakes <fjakis@gmail.com>
2020-07-23 10:28:25 -0300

    criado a tela de editar, já com a funcionalidade do icone pra traser os dados do produto com base no id, o button do cancelar esta com a funcionalidade de retorna a tela de visualização de produto

--- frontend/src/app/components/product/product-updata/product-updata.component.ts
+++ frontend/src/app/components/product/product-updata/product-updata.component.ts
@@ -1,3 +1,6 @@
+import { Product } from './../product.model';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdataComponent implements OnInit {
 
-  constructor() { }
+  product: Product
+  // 1º passo pra fazer com que os dados venham p poder alterar
+  // e em seguida faz a injeção no constructor "private route: ActivatedRoute"
+  // e no iniciar ngOnInit, já coloca "this.productService.readById;"
+  // depois faz a complementação como diz os proximos comentários
+  
+  
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+const id = this.route.snapshot.paramMap.get('id')
+    this.productService.readById(id).subscribe(product =>{
+      this.product = product
+    });
+
+  }
+
+  updateProduct(): void {
+
+  }
+  cancel(): void {
+    this.router.navigate(['/products'])
   }
 
 }
